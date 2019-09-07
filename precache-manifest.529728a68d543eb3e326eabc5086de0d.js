ab33287d9d75ee6e288512d2aebd2912d45ae04c
Milton De León <miltondeleon1996@gmail.com>
2019-09-07 17:40:56 -0600

    Updates

---
+++ precache-manifest.529728a68d543eb3e326eabc5086de0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab3dc9d448aa69649c6135f6af3cc6f",
+    "url": "/practice-pokeapi/index.html"
+  },
+  {
+    "revision": "1bc0b249526b809b170b",
+    "url": "/practice-pokeapi/static/css/main.28406494.chunk.css"
+  },
+  {
+    "revision": "fc744ca3db9768118943",
+    "url": "/practice-pokeapi/static/js/2.73b50835.chunk.js"
+  },
+  {
+    "revision": "1bc0b249526b809b170b",
+    "url": "/practice-pokeapi/static/js/main.d22fd2ae.chunk.js"
+  },
+  {
+    "revision": "d644fde3ca61e2d76734",
+    "url": "/practice-pokeapi/static/js/runtime~main.5d42ae63.js"
+  }
+]);
